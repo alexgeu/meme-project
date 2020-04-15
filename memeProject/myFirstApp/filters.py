3cd4bd2e8b24b729503a412c4c87975a4d8903e5
Malesevic Sasa <sasamalesevic123@gmail.com>
2020-04-15 20:50:18 +0200

    Revert "no message"
    This reverts commit 1f4baa0d71b311d9c37f3d7693d1225d77f7b0fc.

--- memeProject/myFirstApp/filters.py
+++ memeProject/myFirstApp/filters.py
@@ -1,15 +1,8 @@
 import django_filters
-from django_filters import DateFilter, CharFilter
+
 from .models import *
 
 class OrderFilter(django_filters.FilterSet):
-		start_date = DateFilter(label = "start_date", lookup_expr='gte')
-		end_date = DateFilter(label='end_date', lookup_expr='lte')
-		
-		title = CharFilter(label='title', lookup_expr='icontains')
-		
 		class Meta:
 			model = Products
 			fields = '__all__'
-			exlude = ['']
-			
