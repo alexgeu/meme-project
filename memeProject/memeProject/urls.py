2f281a43b63940afe8d972ba2a2ecda132b54d86
Alexander Geuze <alexander@hgn.io>
2020-05-10 23:15:32 +0200

    New navbar, baseX.html is our backup base.html, fixed static file error.

--- memeProject/memeProject/urls.py
+++ memeProject/memeProject/urls.py
@@ -44,4 +44,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    # URL Patterns for profile pics
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # URL Patterns for static files
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
